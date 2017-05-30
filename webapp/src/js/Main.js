3a8366f8afe2720d29e10e8155d71d5976c5a6ca
krassus <krassus@krassus.net>
2017-05-30 17:04:08 +0200

    MainServer update

--- webapp/src/js/Main.js
+++ webapp/src/js/Main.js
@@ -1,16 +1,31 @@
+var request = require("request");
+
+function main() {
+	initMap();
+	loadTours();
+}
+
+function initMap() {
 	let trier = { lat: 49.761784, lng: 6.659463 };
 
 	let options = {
 		zoom: 11,
 		center: trier
 	};
-	function initMap() {
-		new google.maps.Map(document.getElementById("map"), options);
-	}
 
-	function loadTours() {
-		console.log("");
-	}
+	new google.maps.Map(document.getElementById("map"), options);
+}
 
-	initMap();
+function loadTours() {
+	request({
+		url: "http://localhost:8081/routes",
+		json: true
+	}, function (error, response, body) {
+		if (error) {
+			console.log("Failed to fetch routes: " + error);
+		}
+	});
+}
+
+main();
 //Schlüssel: AIzaSyDHMcOZm-IQCO9n9fExRpuOOfc8I32JvvE
