c560611984207f5c066287a490d4fd9841f3154c
krassus <krassus@krassus.net>
2017-06-07 14:01:45 +0200

    package.json updated

--- server/MainServer.js
+++ server/MainServer.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
 
 let PORT = process.argv[2];
@@ -16,9 +17,14 @@ httpServer.get("/tracks", (request, response) => {
 	response.json(JSON.stringify(data));
 });
 
+httpServer.get("/", function (request, response) {
+	console.log("Sending 'index.html'...");
+	response.sendFile(path.resolve(__dirname + "/../webapp/src/release/index.html"));
+});
+
 httpServer.get(/^(.+)$/, function (request, response) {
 	console.log("Sending '" + request.params[0] + "'...");
-	response.sendFile(__dirname + request.params[0]);
+	response.sendFile(path.resolve(__dirname + "/../webapp/src/release" + request.params[0]));
 });
 
 //Express-Server auf Port 8080 starten
