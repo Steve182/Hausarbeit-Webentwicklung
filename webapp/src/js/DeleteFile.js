35a31e83e9b5e64272d147621149f2680108a038
Steve182 <stefanf95@t-online.de>
2017-06-14 16:13:25 +0200

    auf Tracks zoomen hinzugefügt

--- webapp/src/js/DeleteFile.js
+++ webapp/src/js/DeleteFile.js
@@ -1,7 +1,7 @@
 let deleteModule = require("delete");
 let file = process.argv[2];
 
-deleteModule.sync(file, function (error, file) {
+deleteModule.sync(file, function (error) {
 	if (error) {
 		console.error(error);
 	}
--- webapp/src/js/Main.js
+++ webapp/src/js/Main.js
@@ -1,8 +1,11 @@
 let request = require("request");
 let google = require("google-maps");
 
+let liste = document.getElementById("elementList");
+
 let map;
 let route;
+let bounds;
 
 function main() {
 	//Schlüssel zur Nutzung der Google API
@@ -14,9 +17,13 @@ function main() {
 			zoom: 11,
 			center: trier
 		};
-		//neues Map-Objekt mit Trier als Startpunkt erzeugen und in das Element mit der di "map" setzen
+		//neues Map-Objekt mit Trier als Startpunkt erzeugen und in das Element mit der id "map" setzen
 		map = new g.maps.Map(document.getElementById("map"), options);
 
+		//bounds initialisieren
+		bounds = new g.maps.LatLngBounds();
+
+		//route initialisieren (path wird später bei Aufruf eines Tracks gesetzt)
 		route = new g.maps.Polyline({
 			path: null,
 			geodesic: true,
@@ -32,21 +39,39 @@ function main() {
 	loadTours();
 }
 
-let liste = document.getElementById("elementList");
-
+//globalen click-Listener an Liste anmelden
 liste.onclick = function (event) {
 	let id = event.target.getAttribute("id");
 	request({
+		//nur ausgewählten Track laden
 		url: "http://localhost:8080/api/tracks:" + id,
 		json: true
-	}, function (error, response, body) {
+	}, function (error, response) {
 		if (error) {
 			console.log("Failed to fetch route " + id + ": " + error);
 		}
 		else {
 			//Antwort vom Server in JSON parsen
 			let data = JSON.parse(JSON.stringify(response.body));
-			route.setPath(parseCoords(data.features[0].geometry.coordinates));
+
+			//aus erhaltener JSON richtige Koordinaten basteln
+			let coords = parseCoords(data.features[0].geometry.coordinates);
+
+			//Koordinaten des gewählten Tracks als Pfad der Polyline setzen
+			route.setPath(coords);
+
+			//bounds neu initialisieren, da ansonsten die Bounds der vorher ausgewählten Tracks noch mitgenommen werden
+			google.load(function (g) {
+				bounds = new g.maps.LatLngBounds();
+			});
+
+			//bounds setzen (für center und zoom)
+			for (let i = 0; i < coords.length; ++i) {
+				bounds.extend(coords[i]);
+			}
+
+			//richtig zoomen etc.
+			map.fitBounds(bounds);
 		}
 	});
 };
@@ -70,7 +95,7 @@ function loadTours() {
 	request({
 		url: "http://localhost:8080/api/tracks?short=true",
 		json: true
-	}, function (error, response, body) {
+	}, function (error, response) {
 		if (error) {
 			console.log("Failed to fetch routes: " + error);
 		}
@@ -90,4 +115,3 @@ function loadTours() {
 }
 
 main();
-//Schlüssel: AIzaSyDHMcOZm-IQCO9n9fExRpuOOfc8I32JvvE
